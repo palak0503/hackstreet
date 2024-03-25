3426503563d7b58e0dda305515fe5f1cad1c95c2
Samhitha Bodangi <samhithabodangi@gmail.com>
2024-03-25 11:45:20 -0400

    First Commit

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Resources from './pages/Resources.jsx'
+import About from './pages/About.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "resources",
+    element: <Resources />, 
+  },
+  {
+    path: "about",
+    element: <About />, 
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/pages/Resources.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/Header"
+
+function Resources() {
+    return(
+        <div>
+            <Header />
+            <h1>Resources</h1>
+        </div>
+    )
+}
+
+export default Resources
